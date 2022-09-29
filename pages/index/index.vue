7690854c5750e47a17e327fcffbb80c9236ded94
ShenXin <3083387764@qq.com>
2022-09-29 20:14:39 +0800

    3 for remove store

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<view>
+	<view class="home">
 		首页
 	</view>
 </template>
@@ -21,29 +21,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
