f6d8a32f8a91787922c4e729c3f843947b2f2585
ShenXin <3083387764@qq.com>
2022-09-29 18:13:59 +0800

    2 commit for NiceApp increase store

---
+++ store/index.js
@@ -0,0 +1,28 @@
+// #ifndef VUE3
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store({
+			// #endif
+
+			// #ifdef VUE3
+			import {
+				createStore
+			} from 'vuex'
+			const store = createStore({
+				// #endif
+				state: {
+
+				},
+				mutations: {
+
+				},
+				getters: {
+
+				},
+				actions: {
+
+				}
+			})
+
+			export default store
