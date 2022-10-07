a5cd7942ec6ea15e0ff964fddb94d4472dd6207d
wenxuan <1595159972@qq.com>
2022-10-07 21:51:41 +0800

    修改库

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 // 验证规则
-const Joi = require('@hapi/joi')
+const Joi = require('joi')
 const cors = require('cors')
 // 配置文件
 const config = require('./config')
