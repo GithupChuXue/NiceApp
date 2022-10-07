4146b15e8d4e9780c24be196fbec5fad4c9ca775
zza <1137512547@qq.com>
2022-10-07 16:57:32 +0800

    修复搜索结果页错误

--- pages/result/result.vue
+++ pages/result/result.vue
@@ -4,60 +4,60 @@
 		<!-- 返回首页 -->
 		<view class="back">
 			<!-- <image src="../../static/icon/back.png"></image> -->
-				返回
+			返回
 		</view>
 		
-		<!-- 预览展示区域 -->
-		<view class="results">
+		<!-- 搜索结果展示区域 -->
+		<view class="show">
 			<uni-list>
 				<!-- to 属性携带参数跳转详情页面，当前只为参考 -->
-				<uni-list-item direction="column" v-for="item in results" :key="item.id">
+				<uni-list-item direction="column" v-for="(item,index) in list" :key="item.shareid">
 					<!-- 通过header插槽定义列表的标题 -->
 					<template v-slot:header>
-						<view class="uni-note">{{item.publisher}}    {{item.pub_time}}</view>
+						<view class="uni-note">{{item.publisher}} {{item.time}}</view>
 					</template>
 					<!-- 通过body插槽定义列表内容显示 -->
 					<template v-slot:body>
 						<view class="uni-list-box">
 							<view class="uni-content">
-								<view class="uni-title-sub uni-ellipsis-2">{{item.excerpt}}</view>
-							</view>	
+								<view class="uni-title-sub uni-ellipsis-2">{{item.title}}</view>
+							</view>
 							<view class="uni-thumb">
 								<!-- 当前判断长度只为简单判断类型，实际业务中，根据逻辑直接渲染即可 -->
-									<image :src="item.imgs[0]" mode="aspectFill"></image>
+								<image :src="require(`../../api/image/${item.img}`)" mode="aspectFill"></image>
 							</view>
 						</view>
 					</template>
 					<!-- 同步footer插槽定义列表底部的显示效果 -->
 					<template v-slot:footer>
-						<view class="uni-footer">
-							<text class="uni-footer-text" @click="ThumbsUp(item.id)">点赞</text>
+						<view class=" uni-footer">
+							<text class="uni-footer-text" @click="ThumbsUp(item.shareid)">点赞</text>
 							<uni-popup ref="popup_thumbsup" type="center">点赞！</uni-popup>
-							
-							<text class="uni-footer-text" @click="Collect(item.id)">收藏</text>
+
+							<text class="uni-footer-text" @click="Collect(item.shareid)">收藏</text>
 							<uni-popup ref="popup_collect" type="center">收藏！</uni-popup>
-							
-							<text class="uni-footer-text" @click="Comment(item.id)">评论</text>
+
+							<text class="uni-footer-text" @click="Comment(item.shareid)">评论</text>
 							<uni-popup ref="inputDialog" type="dialog">
 								<uni-popup-dialog type="center" mode="input" @confirm="confirm">
 								</uni-popup-dialog>
 							</uni-popup>
-							
-							<text class="uni-footer-text" @click="Share(item.id)">分享</text>
+
+							<text class="uni-footer-text" @click="Share(item.shareid)">分享</text>
 							<uni-popup ref="share" type="share" safeArea backgroundColor="#fff">
 								<uni-popup-share title="分享到" @select="select">
 								</uni-popup-share>
 							</uni-popup>
-							
+
 						</view>
 					</template>
 				</uni-list-item>
 			</uni-list>
 			<!-- 通过loadMore 组件实现上拉加载效果，如需自定义显示内容，可参考：https://ext.dcloud.net.cn/plugin?id=29 -->
 			<!-- <uni-load-more v-if="loading || options.status === 'noMore'" :status="options.status" /> -->
-			<uni-load-more :status="status" :icon-size="10" :content-text="contentText" v-if="this.status!='more'"/>
+			<uni-load-more :status="status" :icon-size="10" :content-text="contentText" v-if="this.status!='more'" />
 		</view>
-
+	
 	</view>
 </template>
 
@@ -85,13 +85,13 @@
 		
 		data() {
 			return {
-				results: read.read_results("../static/data_test/data_local.json"),
 				status: "more",
 				contentText: {
 					contentdown: "更多精彩",
 					contentrefesh: "加载中",
 					contentnomore: "等待更多精彩内容"
-				}
+				},
+				token: uni.getStorageSync("token"),
 			}
 		},
 		
@@ -100,7 +100,9 @@
 				this.$refs.popup_thumbsup[id].open()
 			},
 			Collect(id) {
-				this.$refs.popup_collect[id].open()
+				this.$refs.popup_collect[id].open();
+				//派发收藏的action
+				this.$store.dispatch("getStar", shareid)
 			},
 			Comment(id) {
 				this.$refs.inputDialog[id].open()
@@ -113,26 +115,20 @@
 			}
 		},
 		
+		computed: {
+			list() {
+				return this.$store.state.List;
+			}
+		},
+		
 		//下拉刷新回调函数
 		onPullDownRefresh() {
-			console.log("上拉刷新");
-			this.results = read.read_local("../static/data_test/data_local.json");
-			console.log("上拉刷新已完成");
+			this.$store.dispatch("getAllinInfo");
 		},
 		
 		//上拉加载回调函数
 		onReachBottom() {
 			console.log("上拉加载");
-			var temp = read.read_local("../static/data_test/data_local.json");
-			if (temp){
-				this.status = "loading";
-				this.results = this.results.concat(temp);
-			}
-			else{
-				this.status = "noMore";
-				console.log("没有新内容了");
-			}
-			console.log("上拉加载已完成");
 		}
 	}
 </script>
